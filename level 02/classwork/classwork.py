bbf178fdd3d66ff744d638fd9c9d87a60d7fa38e
VatoTsaishvili <vatotsaishvili@gmail.com>
2024-10-12 20:56:37 +0400

    GOA

---
+++ level 02/classwork/classwork.py
@@ -0,0 +1,14 @@
+print("vato")
+print("tsaishvili")
+print("15")
+
+
+name="vato"
+surname="tsaishvili"
+age=15
+
+print(name)
+print(surname)
+print(age)
+
+print(name + " " + surname + " " + str(age))
