9b376e0c88d1c3a6377b52b536ca78f7c10e3a3f
VatoTsaishvili <vatotsaishvili@gmail.com>
2024-10-17 00:55:11 +0400

    GOA

--- level 02/homework/homework.py
+++ level 02/homework/homework.py
@@ -1,8 +1,25 @@
 name="vato"
 surname="tsaishvili"
 age=15
-
+country = "Georgia"
+city = "zugdidi"
 
 print(name)
 print(surname)
 print(age)
+print(country)
+print(city)
+
+print(name + " " + surname + " " + str(age) + " " + country + " " + city )
+
+print("This is the small information about me")
+print("I love football,")
+print("bicycling with friends,")
+print("i love extreme stunts")
+print("also lerning the programing")
+print("in the best academy there is in the world")
+print("i want to become a best full-stack developer")
+print("I love my family")
+print("because they gave me pass to study")
+print("what i want")
+
---
+++ level 04/classwork/classwork.py
@@ -0,0 +1,55 @@
+country = "Georgia"
+city ="Tbilisi"
+
+num = 3000000
+code =1234
+
+float = 3.5
+float1 = 4.6
+
+print(country)
+print(city)
+
+print(num)
+print(code)
+
+print(float)
+print(float1)
+
+print(5 + 4)
+print(5 + 5)
+print(5 + 6)
+print(2 + 3)
+print(6 + 4)
+
+print(10 - 6)
+print(10 - 5)
+print(10 - 4)
+print(9 - 5)
+print(7 - 2)
+
+print(2 * 5)
+print(3 * 5)
+print(4 * 5)
+print(5 * 4)
+print(3 * 3)
+
+print(10 / 2)
+print(15 / 3)
+print(15 / 2)
+print(20 / 2)
+print(20 / 5)
+
+print(10 % 3)
+print(15 % 6)
+print(13 % 4)
+print(17 % 5)
+print(11 % 4)
+
+print(4 ** 3)
+print(3 ** 4)
+print(5 ** 2)
+print(4 ** 2)
+print(3 ** 3)
+
+print(((3 + 2) + (10 - 5) + (2 * 5)) + ((10 / 5) + (10 % 3) + (4 ** 3)))
