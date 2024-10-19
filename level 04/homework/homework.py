4e45be6c22551a61f149c1e2bd12535b6319b60f
VatoTsaishvili <vatotsaishvili@gmail.com>
2024-10-19 18:39:03 +0400

    GOA

---
+++ level 04/homework/homework.py
@@ -0,0 +1,40 @@
+print(2 + 3)
+print(4 + 6)
+print(3 + 7)
+
+print(10 - 4)
+print(9 - 4)
+print(7 - 5)
+
+print(2 * 5)
+print(5 * 3)
+print(3 * 3)
+
+print(10 / 5)
+print(20 / 10)
+print(15 / 3)
+
+print(10 % 3)
+print(14 % 3)
+print(11 % 3)
+
+print(2 ** 3)
+print(5 ** 2)
+print(10 ** 3)
+
+celsius = 28
+fahrenheit = 82.4
+
+mom = 38
+dad = 41
+grandmother = 56
+secondgrandmother = 71
+uncle = 37
+
+Mom = 58
+Dad = 61
+Grandmother = 76
+Secondgrandmother = 91
+Uncle = 57
+
+print("გაყოფისას ტერმინალში გვიჩვენებს მის პასუხს ხოლო ნაშთიანი გაყოფისას გაყოფაში დარჩენილ ნაშთს")
