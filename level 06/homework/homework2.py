b30dfc5f5bd39eaffd5d14533d2d67bf9158eefc
VatoTsaishvili <vatotsaishvili@gmail.com>
2024-10-27 19:47:22 +0400

    GOA

--- level 06/homework/homework2.py
+++ level 06/homework/homework2.py
@@ -1,1 +1,4 @@
-#camelCase-
+#2) მოიძიეთ ინფორმაცია case-ბის შესახებ და მიუწერეთ რომელ პროგრამირების ენებში იყენებენ
+#snakecase- ვიყენებთ pyton- ში.
+#camelCase- ვიყენებთ javascript - ში.
+#PascalCase- C#, Java and TypeScript
