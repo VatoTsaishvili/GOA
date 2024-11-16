b46e72889d4fb2ca286e0809c73d056c7abd30f5
VatoTsaishvili <vatotsaishvili@gmail.com>
2024-11-17 02:52:57 +0400

    GOA

---
+++ level 12/homework/homework2,3,5,7.py
@@ -0,0 +1,36 @@
+#2) იმუშავეთ ლოგიკურ ოპერაციებზე: print ბრძანებას გადაეცით 2 ან 3 ბულეანი და ისინი ერთმანეთთან დააკავშირეთ ლოგიკური ოპერატორებით (or / and)
+
+print(True or True)
+print(True and False)
+print(True or False)
+print(True and True)
+
+
+#3) print ბრძანების გამოყენებით ჩამოწერეთ 10 ისეთი მაგალითი, სადაც გაერთიანებული გექნებათ შედარების ოპერაციები და ლოგიკური ოპერაციები. კარგი იქნება ცვლადებსაც თუ გამოიყენებთ და მათში შეინახავთ ბულეანებს ან სხვა მონაცემთა ტიპებს
+
+num1 = 10
+num2 = 15
+
+print(num1 > num2)
+print(num2 > num1)
+print(num1 < num2)
+print(num2 < num1)
+print(num1 >= num2)
+print(num2 >= num1)
+print(num1 <= num2)
+print(num2 <= num1)
+print(num1 == num2)
+print(num2 == num1)
+print(num1 != num2)
+print(num2 != num1)
+
+
+#5) თქვენი სიტყვებით ახსენით რა არის control flow და რამდენი ტექნიკა გააჩნია მას
+
+#control flow - არის სინტაქსი და გააჩნია სამი ტიპი: თანმიმდევრობა, იტერაცია და პირობის შემოცმება.
+
+
+#7) თქვენი სიტყვებით ახსენით რა არის ალგორითმი და დაწერეთ რამდენი წარმოდგენის გზა აქვს მას - representation ways of algorithms
+
+#ალგორითმი მრავალი სახით შეგვიძლია გამოვსახოთ მაგალითად natural language, flow chart, pseudocode  და სხვა.
+
