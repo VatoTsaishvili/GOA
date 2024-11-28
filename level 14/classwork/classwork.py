2781e61d649703274e68bfaae992fc17d2cb4a81
VatoTsaishvili <vatotsaishvili@gmail.com>
2024-11-28 22:41:14 +0400

    GOA

---
+++ level 14/classwork/classwork.py
@@ -0,0 +1,13 @@
+#for i in range(20):
+#    print("vato")
+
+#num1, num2 = int(input("enter number : ")), 
+#int(input("enter number : "))
+
+#for i in range(num1, num2):
+#    print(i**2)
+
+num1 = int(input("enter number: "))
+
+for i in range(0, num1):
+    print(i**2)
---
+++ level 16/classwork/classwork.py
@@ -0,0 +1,16 @@
+#num1 = int(input("enter number: "))
+#sum = 0
+#for i in range(num1):
+#    sum= sum + i 
+#   print(sum)
+
+my_num = "1234"
+user_num = input("Enter your num: ")
+counter = 0
+
+while user_num != my_num:
+    user_num = int(input("Enter your num: "))
+    counter += 1
+
+print("succsesful")
+print("Your guess count:", str(counter))
