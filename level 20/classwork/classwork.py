47fa170b99628c9733901c890942ca906aa7dad3
VatoTsaishvili <vatotsaishvili@gmail.com>
2024-12-13 03:07:59 +0400

    GOA

---
+++ level 20/classwork/classwork.py
@@ -0,0 +1,20 @@
+list = [1, 2.4, "GOA" , 5 > 2, 4, 5, 10 ,6, 7, 8]
+
+n1 = list[0]
+n2 = list[1]
+n3 = list[2]
+n4 = list[3]
+n5 = list[4]
+n6 = list[5]
+n7 = list[6]
+n8 = list[7]
+n9 = list[8]
+n10 = list[9]
+
+num1 =int(input("enter number: "))
+num2 =int(input("enter number: "))
+num3 =int(input("enter number: "))
+
+list1 = [num1, num2, num3]
+
+print(list1)
