87d1be43b5cb8d4716324c7c0c42884b310e1af4
VatoTsaishvili <vatotsaishvili@gmail.com>
2025-01-27 20:43:11 +0400

    GOA

--- level 30/homework/homework.py
+++ level 30/homework/homework.py
@@ -1,8 +1,8 @@
-# ```2) Convert a given sentence to uppercase and print the result.```
+# # ```2) Convert a given sentence to uppercase and print the result.```
 
-word = "Banana"
+# word = "Banana"
 
-print(word.upper())
+# print(word.upper())
 
 # 3) Take a user's name input and display it in uppercase letters.
 
@@ -12,6 +12,18 @@ print(name.upper())
 
 # 4) Create a function that converts a list of lowercase strings to uppercase.\
 
+def lower_to_upper(list):
+
+    result = []
+    for i in list:
+        if i.islower():
+            result.append(i.upper())
+        else:
+            result.append(i)
+    
+    print(result)
+
+lower_to_upper(["Apple", "mangO", "cocnut"])
 
 
 # 5) Convert all the characters of a given sentence to lowercase and print it.
@@ -28,7 +40,12 @@ print(email.lower())
 
 # 7) Write a function that takes a mixed-case string and returns it in all lowercase letters.
 
-# def lowercase(text):
+def lowercase(text):
+
+    print(text.lower())
+
+lowercase("apple cat dog BIG MY naME is VatO")
+
 
 # 8) Capitalize the first letter of a sentence provided by the user.
 
@@ -38,10 +55,12 @@ print(word.capitalize())
 
 # 9) Given a list of lowercase strings, capitalize the first letter of each string.
 
-list1 = ["banana" "apple" "mango"]
+list1 = ["banana", "apple", "mango"]
 
-# print(list1.capitalize())
+for i in range(len(list1)):
+    list1[i] = list1[i].capitalize()
 
+print(list1)
 
 # 10) Create a function that takes a string and returns it with the first letter capitalized.
 
@@ -53,11 +72,166 @@ capitalized("my name is vato")
 
 # 11) Find the position of the first occurrence of the word "Python" in a sentence.
 
-word = "Pyton"
+word = "the best Pyton"
 
 print(word.find("Pyton"))
 
 # 12) Search for a user-specified substring in a provided string and print its starting index.
 
-user = "user-specified"
+string = "hello world"
+user = input("enter word: ")
 
+print(string.find(user))
+
+
+# 13) Write a function to find and return the index of a specified character in a given string.
+
+def find_char(string, char):
+
+    print(string.find(char))
+
+find_char("hello", "o")
+
+
+# 14) Find and print the length of a user-provided string.
+
+user_string = input("enter string: ")
+
+print(len(user_string))
+
+
+# 15) Write a function that takes a list of strings and returns their lengths.
+
+def list_of_string(list):
+    print(len(list))
+
+list_of_string([1, 2, 3, 4 ,5])
+
+# 16) Count the number of times the word "the" appears in a given paragraph.
+
+paragraph = "the the the apple book"
+
+print(paragraph.count("the"))
+
+# 17) Ask the user to input a character and count its occurrences in a given string.
+
+paragraph = "the the the apple book"
+user = input("enter char: ")
+
+
+print(paragraph.count(user))
+
+
+# 18) Create a function that counts and returns the occurrences of a specified word in a text.
+
+def count(text,word):
+    print(text.count(word))
+
+count("hello", "l")
+
+# 19) Find and print the index of the first occurrence of the word "hello" in a given string.
+
+string = "hello my name is vato"
+
+print(string.find("hello"))
+
+# 20) Write a function that returns the index of a character provided by the user in a string.
+
+def index_of_character(string,symbol):
+
+    print(string.find(symbol))
+
+index_of_character("hello", "e")
+
+
+# 21) Check if all characters in a given string are lowercase and print the result.
+
+string = input("enter string: ")
+
+if string.islower():
+    print(string, "is lower")
+else:
+    print(string, "is upper")
+
+
+# 22) Create a function that takes a string and returns True if it is completely in lowercase, otherwise False.
+
+def t_or_f(string):
+    print(string.islower())
+
+t_or_f("hello")
+
+# 23) Prompt the user to input a string and verify if it contains only lowercase letters.
+
+string = input("enter string: ")
+
+if string.islower():
+    print(string, "is lower")
+else:
+    print(string, "is upper")
+
+
+# 24) Verify if all the characters in a user-provided string are uppercase.
+
+string = input("enter string: ")
+
+if string.islupper():
+    print(string, "is upper")
+else:
+    print(string, "is lower")
+
+# 25) Write a function that checks if a string consists entirely of uppercase letters and returns a boolean result.\
+
+def check(string):
+        print(string.isupper())
+
+check("HELLO")
+
+
+# 26) Check and display whether a string input by the user is in uppercase.
+
+string = input("enter string: ")
+
+if string.islupper():
+    print(string, "is upper")
+else:
+    print(string, "is lower")
+
+
+# 27) Replace all occurrences of the word "dog" with "cat" in a given sentence.
+
+word = "i love my dog"
+
+print(word.replace("dog", "cat"))
+
+# 28) Write a function that replaces all spaces in a string with underscores.
+
+def replaces(string):
+    print(string.replace(" ", "_"))
+
+replaces("hello world")
+
+# 29) Convert a string such that uppercase letters become lowercase and vice versa, then print the result.
+
+string = input("enter string: ")
+
+if string.isupper():
+    print(string.lower())
+else:
+    print(string.upper())
+
+# 30) Write a function that takes a sentence and returns it with swapped case for each letter.
+
+def swap_cases(string):
+
+    result = ""
+    for i in string:
+        if i.isupper():
+            result += i.lower()
+        else:
+            result += i.upper()
+    print(result)
+
+swap_cases("HeLlO")
+
+        
--- level 32/homework/homework.py
+++ level 32/homework/homework.py
@@ -7,32 +7,24 @@
 
 def user(name,age):
 
-    name = input("enter your name: ")
-    age = input("enter your age: ")
-
     print(f"My name is {name}, i am {age} years old.")
 
-user("name","age")
+user("vato","15")
 
 
 # 4) Write a function that takes a first name and last name, capitalizes them, and formats them into a single string.
 
 def user(firstname,lastname):
 
-
-    firstname = input("enter your firstname: ")
-    lastname = input("enter your lastname: ")
-
     print(f"My firsname is {firstname.capitalize()} and lastname {lastname.capitalize()}")
 
-user("firstname","lastname")
+user("vato","tsaishvili")
 
 
 # 5) Create a function that takes a string, reverses it, and formats it within a sentence.
 
 def reverse_sting(string):
 
-    string = input("enter string: ")
     reversed_string = string[::-1]
 
     print(f"Reversed string is {reversed_string}")
@@ -42,15 +34,13 @@ reverse_sting("string")
 
 # 6) Write a function that takes a sentence, a word, and an index, and inserts the word into the sentence at the given index.
 
-# def insert_word(sentence, word, index):
+def insert_word(sentence, word, index):
 
-#     sentence = input("enter sentence: ")
-#     word = input("enter word: ")
-#     index = input("enter index: ")
+    sentence2 = sentence.split()
+    sentence2.insert(index,word)
+    print(" ".join(sentence2))
 
-
-
-# insert_word("sentence","word","index")
+insert_word("hello my name is vato","word",0)
 
 
 # 7) Write a function that takes a sentence and returns a list of words.
@@ -80,20 +70,68 @@ csv_list("apple, banana, mango, coconut")
 def split_paragraph(paragraph):
 
     sentences = paragraph.split('.')
-                                                # ?
+                                                
     print(sentences)
 
-split_paragraph("hello")
+split_paragraph("hello my name is vato. i am 15 years old")
 
 
 # ```10) Write a function that takes a list and an item, and appends the item to the list.```
 
-# def append_item_to_list(my_list, item):
+def append_item_to_list(my_list, item):
 
-#     my_list = input("enter list: ")
-#     item = input("enter append itme: ")
+    my_list.append(item)
+    print(my_list)
 
-#     my_list.append(item)
-#     print(my_list)
+append_item_to_list([1, 2, 3, 4, 5], "item")
 
-# append_item_to_list("my_list", "item")
+
+# 11) Create a function that takes a list and a list of items, and appends each item to the original list.
+
+def original_list(list1,list2):
+
+    for i in list2:
+        list1.append(i)
+
+    print(list1)
+
+original_list([1, 2, 3, 4, 5],[5, 4, 3, 2, 1])
+
+# 12) Create a function that appends all elements of one list to another list.
+
+def append_elements(list1,list2):
+
+    for i in list2:
+        list1.append(i)
+
+    print(list1)
+
+append_elements([1, 2, 3, 4, 5],[5, 4, 3, 2, 1])
+
+# 13) Write a function that takes a list, an index, and an item, and inserts the item at the specified index.
+
+def insert_items(list, index, item):
+
+    list.insert(index,item)
+    print(list)
+
+insert_items([1, 2, 3, 4, 5], 2, "item")
+
+# 14) Create a function that inserts an item at the beginning of a list.
+
+def inserts(list,item):
+    
+    list.insert(0,item)
+    print(list)
+
+inserts([1, 2, 3, 4, 5], "item")
+
+
+# 15) Create a function that inserts an item at the end of a list using the insert method.
+
+def insert_at_the_end(list,item):
+    
+    list.insert(len(list),item)
+    print(list)
+
+insert_at_the_end([1, 2, 3, 4, 5], "item")
