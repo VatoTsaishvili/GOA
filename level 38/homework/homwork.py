6e4d2f6203b77fb2f44726a8af9f6d0a59872386
VatoTsaishvili <vatotsaishvili@gmail.com>
2025-02-16 00:25:07 +0400

    GOA

--- level 38/homework/homwork.py
+++ level 38/homework/homwork.py
@@ -48,6 +48,12 @@ set1.add(6)
 
 set1.remove(3)
 
+present = 20 in set1
+
+print(set1)
+print(present)
+
+
 # 7) Set Union & Intersection: Create two sets with some common elements and perform union, intersection, and difference operations.
 
 set_a = {1, 2, 3, 4, 5, 6}
